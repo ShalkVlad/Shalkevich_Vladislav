1459e95ece90d4073b135688f5558730f369ce76
ShalkVlad <117597657+ShalkVlad@users.noreply.github.com>
2023-04-20 01:18:09 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+
+
+class CustomLoginView(LoginView):
+    template_name = 'hello/panel.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['username'] = self.request.user.username
+            context['logout_url'] = reverse_lazy('logout')
+        return context
+
+
+def hello(request):
+    return render(request, 'hello/TITLE.html')
+
+
+def parsing_order(request):
+    return render(request, 'hello/Parsing_order.html')
+
+
+def feedback(request):
+    return render(request, 'hello/Feedback.html')
