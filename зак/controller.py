6fbc3b4456b5b1ffc49f5e0b56fb92f3a584c7fa
Shalkevich Vladislav <shalkevichvladislav@gmail.com>
2023-05-15 17:41:55 +0300

    end

--- зак/controller.py
+++ зак/controller.py
@@ -19,30 +19,40 @@ def calculate():
         '6': {'name': 'Контекстная реклама', 'price': 1200},
     }
 
-    service_id = request.form.get('service')
+    service_id = request.form.get('services')
     duration = int(request.form.get('duration'))
     extra = int(request.form.get('extra', 0))
     profit = int(request.form.get('profit'))
     competition = int(request.form.get('competition', 0))
-    employee = request.form.get('employee', 0)
-    new_client = request.form.get('new_client', 0)
+    employee = request.form.get('employee_answer') == 'yes'
+    new_client = request.form.get('client_status') == 'new'
 
     service = services.get(service_id, {'name': 'default_service', 'price': 0})
     price = service['price'] * duration
 
-    if duration >= 4 and duration <= 6:
+    if 1 <= duration <= 3:
+        discount = 0
+    elif 4 <= duration <= 6:
         price *= 0.95
-    elif duration >= 7 and duration <= 12:
+        discount = 0.05 * price
+    elif 7 <= duration <= 12:
         price *= 0.9
-    elif duration >= 13:
+        discount = 0.1 * price
+    else:
         price *= 0.85
+        discount = 0.15 * price
 
-    if competition == 'high':
+    if competition == 1:
         price += 300
-    elif competition == 'medium':
+        competition_str = 'high'
+    elif competition == 2:
         price += 200
-    elif competition == 'low':
+        competition_str = 'medium'
+    elif competition == 3:
         price += 100
+        competition_str = 'low'
+    else:
+        competition_str = 'unknown'
 
     if employee:
         price -= 50
@@ -51,24 +61,17 @@ def calculate():
         price *= 0.9
 
     if extra > 0:
-        price *= 1.2
+        price += 0.2 * price
 
     total_profit = profit * 0.01 * 0.3 * duration
-    discount = 0
-    if employee:
-        discount += 50
-    if new_client:
-        discount += 0.1 * price
-
     final_price = price - discount
-
-    # вычисляем сумму доходов и сумму услуги
-    total_service_price = price + competition + extra
+    total_service_price = price + (300 if competition_str
+    == 'high' else 200 if competition_str == 'medium' else 100 if competition_str == 'low' else 0) + extra
 
     jsonify({
         'service': service['name'],
         'duration': duration,
-        'competition': competition,
+        'competition': competition_str,
         'employee': employee,
         'new_client': new_client,
         'price': price,
@@ -82,7 +85,12 @@ def calculate():
     })
 
     # передача переменной total_service_price в шаблон result.html
-    return render_template('result.html', price=final_price, total_service_price=total_service_price)
+    service_name = service['name']
+    service_price = service['price']
+    other = total_service_price - service_price
+
+    return render_template('result.html', service_name=service_name, service_price=service_price,
+                           total_service_price=total_service_price, other = other)
 
 
 if __name__ == '__main__':
