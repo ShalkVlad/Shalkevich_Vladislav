b232ee323332f5b76d966f1a180aade5fa80dc85
Shalkevich Vladislav <shalkevichvladislav@gmail.com>
2023-07-03 21:14:46 +0300

    good

--- Test/main.py
+++ Test/main.py
@@ -1,6 +1,8 @@
+import os
 import requests
 import pandas as pd
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 
 class Plotter:
@@ -35,6 +37,37 @@ class Plotter:
         else:
             print(f"Failed to download JSON file. Status code: {response.status_code}")
 
+    @staticmethod
+    def draw_plots(json_file):
+        df = pd.read_json(json_file)
+
+        # Create the "plots" folder if it doesn't exist
+        if not os.path.exists("plots"):
+            os.makedirs("plots")
+
+        Plot_paths = []  # Store paths of all generated plots
+
+        # Compare different columns and draw plots
+        columns_to_compare = ['mean', 'max', 'min', 'floor_mean', 'floor_max', 'floor_min',
+                              'ceiling_mean', 'ceiling_max', 'ceiling_min']
+
+        for column in columns_to_compare:
+            plt.figure(figsize=(10, 6))
+            plt.plot(df['name'], df[column])
+            plt.xlabel('Name')
+            plt.ylabel(column.capitalize())
+            plt.title(f'{column.capitalize()} Comparison')
+            plt.xticks(rotation=90)
+
+            # Save the plot with a unique filename
+            plot_path = f'plots/{column}_plot.png'
+            plt.savefig(plot_path)
+            Plot_paths.append(plot_path)
+
+            plt.close()
+
+        return Plot_paths
+
 
 json_file_url = 'https://ai-process-sandy.s3.eu-west-1.amazonaws.com/purge/deviation.json'
 json_file_path = 'deviation.json'
@@ -43,5 +76,12 @@ output_file_path = 'output.xlsx'
 # Download JSON file
 Plotter.download_json(json_file_url, json_file_path)
 
-# Call function and save the result
+# Call function to draw table and save the result
 Plotter.draw_table(json_file_path, output_file_path)
+
+# Call the draw_plots method and get the plot paths
+plot_paths = Plotter.draw_plots(json_file_path)
+
+# Print the paths of all generated plots
+for path in plot_paths:
+    print(f"Plot saved: {path}")
